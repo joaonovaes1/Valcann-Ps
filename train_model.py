4928edacf85e42d31de28c22ecf9c59035feea94
joaonovaes1 <joaomnovaes1998@gmail.com>
2025-09-13 19:11:11 -0300

    remove nan

--- train_model.py
+++ train_model.py
@@ -63,9 +63,11 @@ df['dia_da_semana'] = df.apply(preencher_dia_da_semana, axis=1)
 # Agora aplicar o map para converter texto para inteiro
 df['dia_da_semana'] = df['dia_da_semana'].map(dias_semana)
 
-df['dia_da_semana'].dropna(inplace=True)
-# Converter para inteiro, agora sem nulos
+df = df[df['dia_da_semana'].notna()]
+
+# Agora pode converter para int sem erro
 df['dia_da_semana'] = df['dia_da_semana'].astype(int)
+
 
 
 # Em Promoção
