004a76f4134e5e407815eb04076c9368dd2029ef
joaonovaes1 <joaomnovaes1998@gmail.com>
2025-09-14 22:04:42 -0300

    predicted deleted

--- app.py
+++ app.py
@@ -22,13 +22,3 @@ app = FastAPI()
 @app.get("/")
 def read_rmse():
     return {"message": "API raiz funcionando"}
-
-
-@app.post("/predict")
-def predict(data: PredictRequest):
-    try:
-        features_array = np.array(data.features).reshape(1, -1)
-        prediction = model.predict(features_array)
-        return {"prediction": prediction[0]}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
